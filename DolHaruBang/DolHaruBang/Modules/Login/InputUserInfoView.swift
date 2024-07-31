2d4ff075be9871e053905dad040a16239c1a05a5
yonggari0821 <yonggari0821@naver.com>
2024-07-31 09:46:05 +0900

    Feat: NavigationStack에서 NavigationView로 수정

--- DolHaruBang/DolHaruBang/Modules/Login/InputUserInfoView.swift
+++ DolHaruBang/DolHaruBang/Modules/Login/InputUserInfoView.swift
@@ -42,149 +42,147 @@ struct InputUserInfoView: View {
     }
 
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color.white
-                    .edgesIgnoringSafeArea(.all)
-                
-                GeometryReader { geometry in
-                    VStack {
-                        Spacer()
+        ZStack {
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            
+            GeometryReader { geometry in
+                VStack {
+                    Spacer()
+                    
+                    Text("돌하루방에서 사용할\n닉네임을 정하고\n생일을 입력해주세요.")
+                        .font(.customFont(Font.subtitle2))
+                        .foregroundColor(.mainBlack)
+                        .multilineTextAlignment(.center)
+                        .lineSpacing(8)
+                        .padding(.top, 50)
+                        .padding(.bottom, 50)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        CustomTextField(
+                            text: $name,
+                            placeholder: "닉네임",
+                            font: .customFont(Font.button1)
+                        )
+                        .frame(width: geometry.size.width * 0.6, height: 48)
+                        .cornerRadius(24)
                         
-                        Text("돌하루방에서 사용할\n닉네임을 정하고\n생일을 입력해주세요.")
-                            .font(.customFont(Font.subtitle2))
-                            .foregroundColor(.mainBlack)
-                            .multilineTextAlignment(.center)
-                            .lineSpacing(8)
-                            .padding(.top, 50)
-                            .padding(.bottom, 50)
-                        
-                        Spacer()
-                        
-                        HStack {
-                            CustomTextField(
-                                text: $name,
-                                placeholder: "닉네임",
-                                font: .customFont(Font.button1)
-                            )
-                            .frame(width: geometry.size.width * 0.6, height: 48)
-                            .cornerRadius(24)
-                            
-                            CustomButton(
-                                title: "중복확인",
-                                font: .customFont(Font.button1),
-                                textColor: .white,
-                                action: {
-                                    checkUsername()
-                                }
-                            )
-                            .background(name.isEmpty ? Color.disabled : Color.mainGreen)
-                            .frame(width: geometry.size.width * 0.25, height: 48)
-                            .cornerRadius(24)
-                            .disabled(name.isEmpty)
-                            .onTapGesture {
+                        CustomButton(
+                            title: "중복확인",
+                            font: .customFont(Font.button1),
+                            textColor: .white,
+                            action: {
                                 checkUsername()
                             }
-                        }
-                        .padding(.horizontal, 20)
-                        
-                        HStack {
-                            CustomYearButton(
-                                selectedYear: $selectedYear,
-                                isPresented: $showYearPicker,
-                                font: .customFont(Font.button1)
-                            )
-                            .background(Color.mainGray)
-                            .frame(width: geometry.size.width * 0.27, height: 48)
-                            .cornerRadius(24)
-                            .onTapGesture {
-                                self.showYearPicker = true
-                            }
-                            .sheet(isPresented: $showYearPicker) {
-                                YearPicker(selectedYear: $selectedYear, isPresented: $showYearPicker, years: Array(1900...currentYear)) {
-                                    self.selectedMonth = 1
-                                    self.selectedDay = 1
-                                }
-                            }
-                            
-                            CustomMonthButton(
-                                selectedMonth: $selectedMonth,
-                                isPresented: $showMonthPicker,
-                                font: .customFont(Font.button1)
-                            )
-                            .background(Color.mainGray)
-                            .frame(width: geometry.size.width * 0.27, height: 48)
-                            .cornerRadius(24)
-                            .onTapGesture {
-                                self.showMonthPicker = true
-                            }
-                            .sheet(isPresented: $showMonthPicker) {
-                                let months = selectedYear == currentYear ? Array(1...currentMonth) : Array(1...12)
-                                MonthPicker(selectedMonth: $selectedMonth, isPresented: $showMonthPicker, months: months) {
-                                    self.selectedDay = 1
-                                }
-                            }
-                            
-                            CustomDayButton(
-                                selectedDay: $selectedDay,
-                                isPresented: $showDayPicker,
-                                font: .customFont(Font.button1)
-                            )
-                            .background(Color.mainGray)
-                            .frame(width: geometry.size.width * 0.27, height: 48)
-                            .cornerRadius(24)
-                            .onTapGesture {
-                                self.showDayPicker = true
-                            }
-                            .sheet(isPresented: $showDayPicker) {
-                                DayPicker(selectedDay: $selectedDay, isPresented: $showDayPicker, days: days)
-                            }
-                        }
-                        .padding(.horizontal, 20)
-                        
-                        HStack {
-                            NavigationLink(destination: DBTIGuideView()) {
-                                ZStack {
-                                    HStack {
-                                        Spacer()
-                                        Text("완료")
-                                            .font(.customFont(Font.button1))
-                                            .foregroundColor(.mainWhite)
-                                        Spacer()
-                                    }
-                                }
-                            }
-                            .frame(width: min(geometry.size.width * 0.9, 450), height: 48)
-                            .background(isNameConfirmed ? Color.mainGreen : Color.disabled) // 닉네임 중복 확인이 완료되었을 때만 활성화
-                            .cornerRadius(24)
-                            .disabled(!isNameConfirmed) // 닉네임 중복 확인이 완료되었을 때만 활성화
-                        }
-                        
-                        Spacer()
-                    }
-                }
-                .alert(isPresented: $showAlert) {
-                    if showConfirmation {
-                        return Alert(
-                            title: Text(alertTitle),
-                            message: Text(alertMessage),
-                            primaryButton: .default(Text("Confirm"), action: {
-                                isNameConfirmed = true // 닉네임 중복 확인 완료
-                            }),
-                            secondaryButton: .cancel()
                         )
-                    } else {
-                        return Alert(
-                            title: Text(alertTitle),
-                            message: Text(alertMessage),
-                            dismissButton: .default(Text("OK"))
-                        )
+                        .background(name.isEmpty ? Color.disabled : Color.mainGreen)
+                        .frame(width: geometry.size.width * 0.25, height: 48)
+                        .cornerRadius(24)
+                        .disabled(name.isEmpty)
+                        .onTapGesture {
+                            checkUsername()
+                        }
                     }
+                    .padding(.horizontal, 20)
+                    
+                    HStack {
+                        CustomYearButton(
+                            selectedYear: $selectedYear,
+                            isPresented: $showYearPicker,
+                            font: .customFont(Font.button1)
+                        )
+                        .background(Color.mainGray)
+                        .frame(width: geometry.size.width * 0.27, height: 48)
+                        .cornerRadius(24)
+                        .onTapGesture {
+                            self.showYearPicker = true
+                        }
+                        .sheet(isPresented: $showYearPicker) {
+                            YearPicker(selectedYear: $selectedYear, isPresented: $showYearPicker, years: Array(1900...currentYear)) {
+                                self.selectedMonth = 1
+                                self.selectedDay = 1
+                            }
+                        }
+                        
+                        CustomMonthButton(
+                            selectedMonth: $selectedMonth,
+                            isPresented: $showMonthPicker,
+                            font: .customFont(Font.button1)
+                        )
+                        .background(Color.mainGray)
+                        .frame(width: geometry.size.width * 0.27, height: 48)
+                        .cornerRadius(24)
+                        .onTapGesture {
+                            self.showMonthPicker = true
+                        }
+                        .sheet(isPresented: $showMonthPicker) {
+                            let months = selectedYear == currentYear ? Array(1...currentMonth) : Array(1...12)
+                            MonthPicker(selectedMonth: $selectedMonth, isPresented: $showMonthPicker, months: months) {
+                                self.selectedDay = 1
+                            }
+                        }
+                        
+                        CustomDayButton(
+                            selectedDay: $selectedDay,
+                            isPresented: $showDayPicker,
+                            font: .customFont(Font.button1)
+                        )
+                        .background(Color.mainGray)
+                        .frame(width: geometry.size.width * 0.27, height: 48)
+                        .cornerRadius(24)
+                        .onTapGesture {
+                            self.showDayPicker = true
+                        }
+                        .sheet(isPresented: $showDayPicker) {
+                            DayPicker(selectedDay: $selectedDay, isPresented: $showDayPicker, days: days)
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    HStack {
+                        NavigationLink(destination: DBTIGuideView()) {
+                            ZStack {
+                                HStack {
+                                    Spacer()
+                                    Text("완료")
+                                        .font(.customFont(Font.button1))
+                                        .foregroundColor(.mainWhite)
+                                    Spacer()
+                                }
+                            }
+                        }
+                        .frame(width: min(geometry.size.width * 0.9, 450), height: 48)
+                        .background(isNameConfirmed ? Color.mainGreen : Color.disabled) // 닉네임 중복 확인이 완료되었을 때만 활성화
+                        .cornerRadius(24)
+                        .disabled(!isNameConfirmed) // 닉네임 중복 확인이 완료되었을 때만 활성화
+                    }
+                    
+                    Spacer()
                 }
-                .contentShape(Rectangle()) // Make the entire view tappable
-                .onTapGesture {
-                    self.hideKeyboard()
+            }
+            .alert(isPresented: $showAlert) {
+                if showConfirmation {
+                    return Alert(
+                        title: Text(alertTitle),
+                        message: Text(alertMessage),
+                        primaryButton: .default(Text("Confirm"), action: {
+                            isNameConfirmed = true // 닉네임 중복 확인 완료
+                        }),
+                        secondaryButton: .cancel()
+                    )
+                } else {
+                    return Alert(
+                        title: Text(alertTitle),
+                        message: Text(alertMessage),
+                        dismissButton: .default(Text("OK"))
+                    )
                 }
+            }
+            .contentShape(Rectangle()) // Make the entire view tappable
+            .onTapGesture {
+                self.hideKeyboard()
             }
         }
     }
