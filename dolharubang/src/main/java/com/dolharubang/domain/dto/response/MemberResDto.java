85f370d0a164f13e8a8bedc8118bf90f04659cce
woojin <qpqpqp0102@naver.com>
2024-08-17 23:21:06 +0900

    Fix: members 복수형에서 단수형으로 변경

---
+++ dolharubang/src/main/java/com/dolharubang/domain/dto/response/MemberResDto.java
@@ -0,0 +1,44 @@
+package com.dolharubang.domain.dto.response;
+
+import com.dolharubang.domain.entity.Member;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+public class MemberResDto {
+
+    private String memberEmail;
+    private String nickname;
+    private String birthday;
+    private String refreshToken;
+    private String provider;
+    private Long sands;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLoginAt;
+    private LocalDateTime modifiedAt;
+    private Long totalLoginDays;
+    private String profilePicture;
+    private String spaceName;
+
+    public static MemberResDto fromEntity(Member member) {
+        return MemberResDto.builder()
+            .memberEmail(member.getMemberEmail())
+            .nickname(member.getNickname())
+            .birthday(member.getBirthday())
+            .refreshToken(member.getRefreshToken())
+            .provider(member.getProvider())
+            .sands(member.getSands())
+            .createdAt(member.getCreatedAt())
+            .lastLoginAt(member.getLastLoginAt())
+            .modifiedAt(member.getModifiedAt())
+            .totalLoginDays(member.getTotalLoginDays())
+            .spaceName(member.getSpaceName())
+            .build();
+    }
+}
