50698fbd364407fc732c233b05d0406fb341be75
smyang0220 <smyang0220@naver.com>
2024-08-22 04:20:23 +0900

    Feat : customizeTab make

--- DolHaruBang/DolHaruBang/Common/Sources/Modules/DolModule/DolView.swift
+++ DolHaruBang/DolHaruBang/Common/Sources/Modules/DolModule/DolView.swift
@@ -8,14 +8,18 @@
 import SceneKit
 import SwiftUI
 
+
+// 3D 돌을 출력하는 UI
 struct DolView : UIViewRepresentable {
     
+//    @Binding var selectedFaceShape : FaceShape
     @Binding var selectedFace : Face
+    @Binding var selectedFaceShape : FaceShape
     
     // UI가 만들어질때 생성
     func makeUIView(context: Context) -> SCNView {
         let scnView = SCNView()
-        scnView.scene = loadScene()
+        scnView.scene = loadScene(faceShape: selectedFaceShape)
         scnView.backgroundColor = UIColor.clear // SCNView의 배경을 투명하게 설정
         scnView.allowsCameraControl = true
         
@@ -38,22 +42,23 @@ struct DolView : UIViewRepresentable {
         } else {
                 print("\(selectedFace) 노드가 씬에 존재하지 않습니다.")
         }
-    }
-
-
-    
+    }  
     
 }
 
-func loadScene() -> SCNScene {
-    let scene = SCNScene(named: "Dols.scnassets/sparkle.scn") ?? SCNScene()
+
+func loadScene(faceShape : FaceShape) -> SCNScene {
+    let scene = SCNScene(named: "Dols.scnassets/\(faceShape).scn") ?? SCNScene()
     
 //        // 너무 커서 그랬다..
-//        for node in scene.rootNode.childNodes {
-//              node.scale = SCNVector3(x: 0.1, y: 0.1, z: 0.1) // 스케일 값을 조정하여 모델의 크기를 조절
-//          }
+        for node in scene.rootNode.childNodes {
+              node.scale = SCNVector3(x: 4, y: 4, z: 4) // 스케일 값을 조정하여 모델의 크기를 조절
+          }
     
     printNodeDetails(node: scene.rootNode)
+    
+    // shading 값 설정
+//    updateMaterialsToPhysicallyBased(for: scene)
     
     let cameraNode = makeCamera()
     scene.rootNode.addChildNode(cameraNode)
@@ -62,18 +67,24 @@ func loadScene() -> SCNScene {
     let ambientLightNode = makeAmbientLight()
     scene.rootNode.addChildNode(ambientLightNode)
       
-      // 방향성 조명 추가
-      let directionalLightNode = makeDirectionalLight()
-      scene.rootNode.addChildNode(directionalLightNode)
+      // 방향성 조명 추가 1
+    let forwardDirectionalLightNode = makeDirectionalLight(X: -0.01, Y: -0.01, Z: 10, intensity: 1000, name: "forwardDirectionalLightNode")
+      scene.rootNode.addChildNode(forwardDirectionalLightNode)
       
-   
+    // 방향성 조명 추가 2
+    let leftDirectionalLightNode = makeDirectionalLight(X: -6, Y: 5, Z: 6.5, intensity: 500, name: "leftDirectionalLightNode")
+    scene.rootNode.addChildNode(leftDirectionalLightNode)
+    
+    // 방향성 조명 추가 3
+    let rightDirectionalLightNode = makeDirectionalLight(X: 6, Y: -3, Z: -3, intensity: 200, name: "rightDirectionalLightNode")
+    scene.rootNode.addChildNode(rightDirectionalLightNode)
     
     return scene
 }
 
 func hideAllNodesExcept(node: SCNNode, rootNode: SCNNode) {
     for childNode in rootNode.childNodes {
-        if childNode != node && childNode.name != "camera"  && childNode.name != "ambientLight" && childNode.name != "directionalLight" {
+        if childNode != node && childNode.name != "camera"  && childNode.name != "ambientLight" && childNode.name != "leftDirectionalLightNode" && childNode.name != "fowardDirectionalLightNode" && childNode.name != "rightDirectionalLightNode" {
             childNode.isHidden = true
         }
     }
@@ -93,7 +104,7 @@ func makeCamera() -> SCNNode {
         let cameraNode = SCNNode()
        cameraNode.camera = SCNCamera()
        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
-       cameraNode.camera?.automaticallyAdjustsZRange = true
+       cameraNode.camera?.automaticallyAdjustsZRange = false
         cameraNode.name = "camera"
         return cameraNode
 }
@@ -102,28 +113,37 @@ func makeAmbientLight() -> SCNNode{
     let ambientLightNode = SCNNode()
     let ambientLight = SCNLight()
     ambientLight.type = .ambient
-    ambientLight.intensity = 1000 // 환경 조명 강도 조절
+    ambientLight.intensity = 3000 // 환경 조명 강도 조절
     ambientLightNode.light = ambientLight
     ambientLightNode.name = "ambientLight"
     return ambientLightNode
 }
 
-func makeDirectionalLight() -> SCNNode {
+func makeDirectionalLight(X: Float, Y : Float, Z : Float, intensity : CGFloat, name : String ) -> SCNNode {
     let directionalLightNode = SCNNode()
     let directionalLight = SCNLight()
     directionalLight.type = .directional
-    directionalLight.intensity = 3000 // 방향성 조명 강도 조절
+    directionalLight.intensity = intensity // 방향성 조명 강도 조절
     directionalLight.color = UIColor.white // 조명 색상 설정
     directionalLight.castsShadow = true // 그림자 생성 설정
     directionalLightNode.light = directionalLight
-    directionalLightNode.name = "directionalLight"
+    directionalLightNode.name = name
     
     // 방향성 조명의 방향 설정
-    directionalLightNode.position = SCNVector3(x: 5, y: 5, z: 10)
-      directionalLightNode.look(at: SCNVector3.init(x: 0, y: 0, z: 0)) // 장면의 중심을 향하도록 조명 방향 설정
+    directionalLightNode.position = SCNVector3(x: X, y: Y, z: Z)
+    directionalLightNode.look(at: SCNVector3.init(x: 0, y: 0, z: 0)) // 장면의 중심을 향하도록 조명 방향 설정
     
     return directionalLightNode
 }
+
+func updateMaterialsToPhysicallyBased(for scene: SCNScene) {
+    scene.rootNode.enumerateChildNodes { (node, _) in
+        for material in node.geometry?.materials ?? [] {
+            material.lightingModel = .physicallyBased
+        }
+    }
+}
+
 
 
 func printNodeDetails(node: SCNNode, depth: Int = 0) {
