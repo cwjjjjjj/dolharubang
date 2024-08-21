7d4264c63399243507234a8d19c56e5de4bbf2b1
smyang0220 <smyang0220@naver.com>
2024-08-22 07:32:25 +0900

    Design : sheet error sol

--- DolHaruBang/DolHaruBang/Pages/Decoration/Source/Modules/CustomizeView.swift
+++ DolHaruBang/DolHaruBang/Pages/Decoration/Source/Modules/CustomizeView.swift
@@ -17,32 +17,38 @@ struct CustomizeView<T: Customizable>: View where T.AllCases == [T] {
     ]
     
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: columns, spacing: 20) { // spacing을 늘려서 항목들 간의 간격을 조정합니다.
-                ForEach(T.allCases, id: \.self) { item in
-                    Button(action: {
-                        item.performAction(with: store)
-                    }) {
-                        VStack(spacing: 15) { // spacing을 조정하여 이미지와 텍스트 간의 간격을 조정합니다.
-                            Image("background")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 108, height: 108)
-                                .clipped()
-                                .clipShape(RoundedRectangle(cornerRadius: 15))
-                            
-                            Text("\(item.description)")
-                                .font(Font.customFont(Font.caption1))
-                                .foregroundColor(Color.decoSheetTextColor)
-                                .padding(.bottom, 5) // 텍스트와 버튼 하단의 여백 조정
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                ScrollView(.horizontal) {
+                    LazyHGrid(rows: columns, spacing: 20) {
+                        ForEach(T.allCases, id: \.self) { item in
+                            Button(action: {
+                                item.performAction(with: store)
+                            }) {
+                                VStack(spacing: 15) {
+                                    Image("plz")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 108, height: 108)
+                                        .clipped()
+                                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                                    
+                                    Text("\(item.description)")
+                                        .font(Font.customFont(Font.caption1))
+                                        .foregroundColor(Color.decoSheetTextColor)
+                                        .padding(.bottom, 5)
+                                }
+                                .frame(width: 120, height: 140)
+                                .padding(5)
+                            }
                         }
-                        .frame(width: 120, height: 130) // 버튼의 전체 크기를 조정하여 겹침 방지
-                        .padding(5) // 버튼의 패딩을 추가하여 여백 확보
                     }
-                    
+                    .padding(.horizontal, 20)
                 }
+                .frame(height: geometry.size.height) // ScrollView의 높이를 GeometryReader의 높이로 설정
+                .background(Color.white)
             }
-            .padding(.horizontal, 20) // ScrollView의 좌우 여백 조정
-        }.frame(height: 300).background(Color.white)
+            .frame(maxWidth: .infinity, maxHeight: .infinity) // VStack의 최대 크기를 무한대로 설정
+        }
     }
 }
