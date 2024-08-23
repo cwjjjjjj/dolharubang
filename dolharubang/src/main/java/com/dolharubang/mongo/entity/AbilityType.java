96196567186ee7854cd42b0aca0738eea71c3ba0
woojin <qpqpqp0102@naver.com>
2024-08-24 04:20:45 +0900

    Feat: AbilityType 파일 작성

---
+++ dolharubang/src/main/java/com/dolharubang/mongo/entity/AbilityType.java
@@ -0,0 +1,21 @@
+package com.dolharubang.mongo.entity;
+
+public enum AbilityType {
+    //오늘의 명언
+    WISESAYING,
+
+    //기상캐스터
+    WEATHERCASTER,
+
+    //Rock스타
+    ROCKSTAR,
+
+    //탄생석 운세
+    FORTUNETELLING,
+
+    //고민해결사
+    ADVISOR,
+
+    //쩝쩝박사
+    FOODEXPERT
+}
