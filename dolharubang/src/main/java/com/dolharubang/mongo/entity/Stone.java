d3f899dca37fd419c87e32f3dbdee266f23e6fc2
woojin <qpqpqp0102@naver.com>
2024-08-24 04:27:36 +0900

    Fix: x, y 좌표 정보는 필요없어졌으므로 클래스 구조 수정

--- dolharubang/src/main/java/com/dolharubang/mongo/entity/Stone.java
+++ dolharubang/src/main/java/com/dolharubang/mongo/entity/Stone.java
@@ -3,10 +3,8 @@ package com.dolharubang.mongo.entity;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Map;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,57 +16,21 @@ public class Stone {
     @Id
     private ObjectId stoneId;
 
-    private String memberId;
+    private Long memberId;
 
-    private long speciesId;
+    private Long speciesId;
 
     private String stoneName;
 
     private LocalDateTime createdAt;
 
-    private long closeness;
+    private Long closeness;
 
-    private Map<String, Boolean> abilityAble;
+    private Map<AbilityType, Boolean> abilityAble;
 
     private String signText;
 
-    private CustomDataEntity custom;
+    //TODO Item 클래스와 매칭되도록 좀 더 고민해볼 것
+    private Map<ItemType, String> custom;
 
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class CustomDataEntity {
-        //배경
-        private Map<String, Map<String, CustomItem>> background;
-
-        //배경 소품
-        private Map<String, Map<String, CustomItem>> backProp;
-
-        //돌받침
-        private Map<String, Map<String, CustomItem>> support;
-
-        //악세사리
-        private Map<String, Map<String, CustomItem>> accs;
-
-        //이목구비
-        private Map<String, Map<String, CustomItem>> facialFeatures;
-
-        //팻말
-        private Map<String, Map<String, CustomItem>> sign;
-
-        //특수아이템
-        private Map<String, Map<String, CustomItem>> specialItem;
-    }
-
-    //TODO x, y 좌표 정보는 필요없음
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class CustomItem {
-        private String url;
-        private Double xCoordinate;
-        private Double yCoordinate;
-    }
 }
