c249925171b9e7f89b817467166e726d9d5eaa45
woojin <qpqpqp0102@naver.com>
2024-08-24 04:26:43 +0900

    Fix: 매개변수 또는 파라미터 수정

--- dolharubang/src/main/java/com/dolharubang/mongo/repository/StoneRepository.java
+++ dolharubang/src/main/java/com/dolharubang/mongo/repository/StoneRepository.java
@@ -2,13 +2,15 @@ package com.dolharubang.mongo.repository;
 
 import com.dolharubang.mongo.entity.Stone;
 import java.util.Optional;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoneRepository extends MongoRepository<Stone, String> {
 
-    Optional<Stone> findById(String id);
+    Optional<Stone> findByStoneId(@Param("stoneId") ObjectId stoneId);
 
-    Optional<Stone>  findByMemberEmail(String memberEmail);
+    Optional<Stone> findByMemberId(@Param("memberId") long memberId);
 }
