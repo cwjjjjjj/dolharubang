8f736c2f8b19828fa74a4d9f41e2860c3b3b7a73
woojin cheon <qpqpqp0102@naver.com>
2024-09-23 21:42:41 +0900

    Feat: 모래알 차감 메서드, 멤버아이템 생성 메서드 추가

--- dolharubang/src/main/java/com/dolharubang/domain/entity/Member.java
+++ dolharubang/src/main/java/com/dolharubang/domain/entity/Member.java
@@ -92,6 +92,10 @@ public class Member {
         this.modifiedAt = LocalDateTime.now();
     }
 
+    public void deductSands(Long price) {
+        this.sands -= price;
+    }
+
     //엔티티가 영속성 컨텍스트에 저장되기 전에 호출
     @PrePersist
     protected void onCreate() {
