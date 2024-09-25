0c829d90f532172affde28a09b66ac8ae034d083
woojin cheon <qpqpqp0102@naver.com>
2024-09-26 04:02:51 +0900

    Feat: 불필요한 의존성 제거, 소셜로그인 관련 의존성 주석 해제

--- dolharubang/build.gradle.kts
+++ dolharubang/build.gradle.kts
@@ -41,15 +41,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation ("org.bgee.log4jdbc-log4j2:log4jdbc-log4j2-jdbc4.1:1.16")
-//    implementation("org.springframework.security:spring-security-oauth2-client:6.3.3")
-//    implementation("org.springframework.boot:spring-boot-starter-security:3.3.4")
-
+    implementation("org.springframework.security:spring-security-oauth2-client:6.3.3")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.3.4")
     implementation("io.awspring.cloud:spring-cloud-starter-aws:2.4.4")
-    compileOnly ("org.springframework.cloud:spring-cloud-starter-aws:2.0.2.RELEASE")
-    implementation (platform("com.amazonaws:aws-java-sdk-bom:1.11.228"))
-    implementation ("com.amazonaws:aws-java-sdk-s3")
     implementation ("commons-io:commons-io:2.7")
-    implementation ("net.coobird:thumbnailator:0.4.14")
 }
 
 configurations.all {
