7c96fa183ee0f6d2cb437a26b8247798e309a3da
woojin cheon <qpqpqp0102@naver.com>
2024-11-20 17:37:41 +0900

    feat(MemberRepository): 소셜로그인 관련 메서드 시그니처 수정
    - socialId에 더해 socialType도 함께 이용해 멤버 검색
    - 리턴 타입 Optional 로 변경

--- dolharubang/src/main/java/com/dolharubang/repository/MemberRepository.java
+++ dolharubang/src/main/java/com/dolharubang/repository/MemberRepository.java
@@ -1,10 +1,10 @@
 package com.dolharubang.repository;
 
 import com.dolharubang.domain.entity.Member;
+import com.dolharubang.type.SocialType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -12,5 +12,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByNicknameContaining(String keyword);
 
-    Member findBySocialId(String socialId);
+    Optional<Member> findBySocialTypeAndSocialId(SocialType socialType,String socialId);
 }
