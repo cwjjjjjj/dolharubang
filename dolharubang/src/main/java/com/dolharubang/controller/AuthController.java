2dcb9c4a5d387fbb711ed9b227cb4c4ccebfb381
woojin cheon <qpqpqp0102@naver.com>
2024-12-15 23:50:34 +0900

    fix: 매핑 주소 수정

--- dolharubang/src/main/java/com/dolharubang/controller/AuthController.java
+++ dolharubang/src/main/java/com/dolharubang/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/oauth2")
 @RestController
 public class AuthController {
 
