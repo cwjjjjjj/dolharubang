018d1ede4d0c7a1d126c13bcfe5298f171fac135
woojin cheon <qpqpqp0102@naver.com>
2025-01-17 00:09:11 +0900

    feat: 모든 회원 조회 메소드 추가

--- dolharubang/src/main/java/com/dolharubang/controller/MemberController.java
+++ dolharubang/src/main/java/com/dolharubang/controller/MemberController.java
@@ -5,8 +5,10 @@ import com.dolharubang.domain.dto.response.MemberResDto;
 import com.dolharubang.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,13 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @Operation(summary = "모든 회원 조회하기")
+    @GetMapping
+    public ResponseEntity<List<MemberResDto>> getMember() {
+        List<MemberResDto> response = memberService.getAllMembers();
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "회원 가입하기 (소셜로그인 아닌, 임시 방식)", description = "회원을 생성한다.")
