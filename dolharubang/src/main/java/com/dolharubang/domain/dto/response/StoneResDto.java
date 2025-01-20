d02b0cbfe40e66bc638e06185ed506da9d03c072
woojin cheon <qpqpqp0102@naver.com>
2025-01-21 02:29:25 +0900

    feat: 돌 입양 메서드, controller, dto 작성

--- dolharubang/src/main/java/com/dolharubang/domain/dto/response/StoneResDto.java
+++ dolharubang/src/main/java/com/dolharubang/domain/dto/response/StoneResDto.java
@@ -1,5 +1,37 @@
 package com.dolharubang.domain.dto.response;
 
+import com.dolharubang.domain.entity.Member;
+import com.dolharubang.domain.entity.Stone;
+import com.dolharubang.mongo.enumTypes.ItemType;
+import com.dolharubang.type.AbilityType;
+import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
 public class StoneResDto {
 
+    private Long stoneId;
+    private Member member;
+    private Long speciesId;
+    private String stoneName;
+    private Long closeness;
+    private Map<AbilityType, Boolean> abilityAble;
+    private String signText;
+    private Map<ItemType, String> custom;
+
+    public static StoneResDto fromEntity(Stone stone) {
+        return StoneResDto.builder()
+            .stoneId(stone.getStoneId())
+            .speciesId(stone.getSpeciesId())
+            .stoneName(stone.getStoneName())
+            .closeness(stone.getCloseness())
+            .abilityAble(stone.getAbilityAble())
+            .signText(stone.getSignText())
+            .custom(stone.getCustom())
+            .build();
+    }
 }
--- dolharubang/src/main/java/com/dolharubang/service/StoneService.java
+++ dolharubang/src/main/java/com/dolharubang/service/StoneService.java
@@ -1,6 +1,9 @@
 package com.dolharubang.service;
 
+import com.dolharubang.domain.dto.request.StoneReqDto;
+import com.dolharubang.domain.dto.response.StoneResDto;
 import com.dolharubang.domain.entity.Member;
+import com.dolharubang.domain.entity.Stone;
 import com.dolharubang.exception.CustomException;
 import com.dolharubang.exception.ErrorCode;
 import com.dolharubang.repository.MemberRepository;
@@ -21,17 +24,35 @@ public class StoneService {
         this.memberRepository = memberRepository;
     }
 
-    //TODO 입양 부분(create) 구현
+    @Transactional
+    public StoneResDto adoptStone(StoneReqDto stoneReqDto) {
+        Member member = memberRepository.findById(stoneReqDto.getMemberId())
+            .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        Stone stone = Stone.builder()
+            .member(member)
+            .speciesId(stoneReqDto.getSpeciesId())
+            .stoneName(stoneReqDto.getStoneName())
+            .closeness(stoneReqDto.getCloseness())
+            .abilityAble(stoneReqDto.getAbilityAble())
+            .signText(stoneReqDto.getSignText())
+            .custom(stoneReqDto.getCustom())
+            .build();
+
+        Stone adoptedStone = stoneRepository.save(stone);
+        return StoneResDto.fromEntity(adoptedStone);
+    }
 
     @Transactional(readOnly = true)
     public String readSignText(Long memberId) {
-        Member member = memberRepository.findByMemberId(memberId)
+        Member member = memberRepository.findById(memberId)
             .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
         String signText = "";
-        signText = stoneRepository.findSignTextByMember(member)
-            .orElseThrow(() -> new CustomException(ErrorCode.SIGNTEXT_NOT_FOUND));
-
+        signText = stoneRepository.findSignTextByMember(member);
+        if(signText == null) {
+            throw new CustomException(ErrorCode.SIGNTEXT_NOT_FOUND);
+        }
         return signText;
     }
 }
