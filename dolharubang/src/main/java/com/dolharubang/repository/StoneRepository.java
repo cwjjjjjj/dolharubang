dc9484d94a83e504c3d9bcc5e8e9acc36d2453f9
woojin cheon <qpqpqp0102@naver.com>
2025-01-23 22:07:22 +0900

    remove: 사용하지 않는 메서드 삭제

--- dolharubang/src/main/java/com/dolharubang/repository/StoneRepository.java
+++ dolharubang/src/main/java/com/dolharubang/repository/StoneRepository.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoneRepository extends JpaRepository<Stone, Long> {
-    Optional<Stone> findByStoneName(String stoneName);
-
     Optional<Stone> findByMember(Member member);
 
     String findSignTextByMember(Member member);
