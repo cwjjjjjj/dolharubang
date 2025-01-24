890874d40008e6da4715a843e0f1bd71067dc524
woojin cheon <qpqpqp0102@naver.com>
2025-01-25 06:38:49 +0900

    feat: Item 전체 조회, 추가, 수정 구현

--- dolharubang/src/main/java/com/dolharubang/mongo/service/ItemService.java
+++ dolharubang/src/main/java/com/dolharubang/mongo/service/ItemService.java
@@ -1,7 +1,11 @@
 package com.dolharubang.mongo.service;
 
+import com.dolharubang.exception.CustomException;
+import com.dolharubang.exception.ErrorCode;
 import com.dolharubang.mongo.entity.Item;
 import com.dolharubang.mongo.repository.ItemRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
@@ -20,5 +24,28 @@ public class ItemService {
         ObjectId objectId = new ObjectId(itemId);
 
         return itemRepository.findByItemId(objectId);
+    }
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    public Item saveItem(Item item) {
+        item.setCreatedAt(LocalDateTime.now());
+        item.setModifiedAt(LocalDateTime.now());
+        return itemRepository.save(item);
+    }
+
+    public Item updateItem(String itemId, Item updateItem) {
+        Item item = findByItemId(itemId)
+            .orElseThrow(() -> new CustomException(ErrorCode.ITEM_NOT_FOUND));
+
+        item.setItemType(updateItem.getItemType());
+        item.setItemName(updateItem.getItemName());
+        item.setImageUrl(updateItem.getImageUrl());
+        item.setPrice(updateItem.getPrice());
+        item.setModifiedAt(LocalDateTime.now());
+
+        return itemRepository.save(item);
     }
 }
