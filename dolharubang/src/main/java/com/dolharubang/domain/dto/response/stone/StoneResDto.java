1b0557655d06993f5e3e415914de7aa5b5ee9918
woojin cheon <qpqpqp0102@naver.com>
2025-01-26 23:37:09 +0900

    feat: 아이템 구매 메서드 파라미터, 로직 변경 / MemberItem 필드 추가 (isSelected)

--- dolharubang/src/main/java/com/dolharubang/domain/dto/response/stone/StoneResDto.java
+++ dolharubang/src/main/java/com/dolharubang/domain/dto/response/stone/StoneResDto.java
@@ -2,7 +2,6 @@ package com.dolharubang.domain.dto.response.stone;
 
 import com.dolharubang.domain.entity.Member;
 import com.dolharubang.domain.entity.Stone;
-import com.dolharubang.mongo.enumTypes.ItemType;
 import com.dolharubang.type.AbilityType;
 import java.util.Map;
 import lombok.Builder;
@@ -21,17 +20,16 @@ public class StoneResDto {
     private Long closeness;
     private Map<AbilityType, Boolean> abilityAble;
     private String signText;
-    private Map<ItemType, String> custom;
 
     public static StoneResDto fromEntity(Stone stone) {
         return StoneResDto.builder()
             .stoneId(stone.getStoneId())
+            .member(stone.getMember())
             .speciesId(stone.getSpeciesId())
             .stoneName(stone.getStoneName())
             .closeness(stone.getCloseness())
             .abilityAble(stone.getAbilityAble())
             .signText(stone.getSignText())
-            .custom(stone.getCustom())
             .build();
     }
 }
