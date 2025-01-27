98daed97e7fa6d752328e144b984908e5d1ed592
안성재 <sammy0329@naver.com>
2025-01-27 20:00:01 +0900

    feat: 특정 contest 삭제 기능 구현

--- dolharubang/src/main/java/com/dolharubang/controller/ContestController.java
+++ dolharubang/src/main/java/com/dolharubang/controller/ContestController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,4 +51,12 @@ public class ContestController {
         return ResponseEntity.ok(
             contestService.updateContestVisibility(memberId, contestId, isPublic));
     }
+
+    @DeleteMapping("/{memberId}/{contestId}")
+    public ResponseEntity<Void> deleteContest(@PathVariable Long memberId,
+        @PathVariable Long contestId) {
+        contestService.deleteContest(memberId, contestId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
