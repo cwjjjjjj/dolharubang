dc0f2c439aa14bd266dbcc57dd95f815920cf7f3
안성재 <sammy0329@naver.com>
2025-01-27 11:01:31 +0900

    feat: contest 엔티티 생성

---
+++ dolharubang/src/main/java/com/dolharubang/domain/entity/Contest.java
@@ -0,0 +1,47 @@
+package com.dolharubang.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "contests")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Contest extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contest_id")
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    Member member;
+
+    @Column(name = "is_public")
+    Boolean isPublic;
+
+    @Column(name = "profile_img_url")
+    String profileImgUrl;
+
+    @Column(name = "stone_name")
+    String stoneName;
+
+    public void updateImage(String profileImgUrl) {
+        this.profileImgUrl = profileImgUrl;
+    }
+
+}
