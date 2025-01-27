7891a735c21012c0a50d91d06be6ae09ee8cf58c
안성재 <sammy0329@naver.com>
2025-01-27 11:04:38 +0900

    feat: contest 서비스 생성

---
+++ dolharubang/src/main/java/com/dolharubang/service/ContestService.java
@@ -0,0 +1,50 @@
+package com.dolharubang.service;
+
+import com.dolharubang.domain.dto.request.ContestReqDto;
+import com.dolharubang.domain.dto.response.ContestResDto;
+import com.dolharubang.domain.entity.Contest;
+import com.dolharubang.domain.entity.Member;
+import com.dolharubang.exception.CustomException;
+import com.dolharubang.exception.ErrorCode;
+import com.dolharubang.repository.ContestRepository;
+import com.dolharubang.repository.MemberRepository;
+import com.dolharubang.s3.S3UploadService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ContestService {
+
+    private final ContestRepository contestRepository;
+    private final MemberRepository memberRepository;
+    private final S3UploadService s3UploadService;
+
+    public ContestService(ContestRepository contestRepository, MemberRepository memberRepository,
+        S3UploadService s3UploadService) {
+        this.contestRepository = contestRepository;
+        this.memberRepository = memberRepository;
+        this.s3UploadService = s3UploadService;
+    }
+
+    @Transactional
+    public ContestResDto createContest(Long memberId, ContestReqDto reqDto) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        Contest contest = Contest.builder()
+            .member(member)
+            .isPublic(reqDto.getIsPublic())
+            .profileImgUrl(reqDto.getProfileImgUrl())
+            .stoneName(reqDto.getStoneName())
+            .build();
+
+        Contest savedContest = contestRepository.save(contest);
+        String imageUrl = s3UploadService.saveImage(reqDto.getProfileImgUrl(),
+            "dolharubang/contest/",
+            savedContest.getId());
+
+        savedContest.updateImage(imageUrl);
+
+        return ContestResDto.fromEntity(savedContest);
+    }
+}
