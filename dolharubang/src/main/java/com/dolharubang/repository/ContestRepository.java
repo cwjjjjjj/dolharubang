2c140be14328487efb7b4e082df49577180ea615
안성재 <sammy0329@naver.com>
2025-01-27 11:04:10 +0900

    feat: contest 레퍼지토리 생성

---
+++ dolharubang/src/main/java/com/dolharubang/repository/ContestRepository.java
@@ -0,0 +1,8 @@
+package com.dolharubang.repository;
+
+import com.dolharubang.domain.entity.Contest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContestRepository extends JpaRepository<Contest, Long> {
+
+}
