01c8d6a7f1b54345a5c5834daa809b19025c3885
안성재 <sammy0329@naver.com>
2025-01-27 15:36:10 +0900

    feat: 멤버의 특정 돌자랑 조회 기능 구현

--- dolharubang/src/main/java/com/dolharubang/repository/ContestRepository.java
+++ dolharubang/src/main/java/com/dolharubang/repository/ContestRepository.java
@@ -3,10 +3,13 @@ package com.dolharubang.repository;
 import com.dolharubang.domain.entity.Contest;
 import com.dolharubang.domain.entity.Member;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContestRepository extends JpaRepository<Contest, Long> {
 
     List<Contest> findAllByMember(Member member);
+
+    Optional<Contest> findByIdAndMember(long contestNo, Member member);
     
 }
