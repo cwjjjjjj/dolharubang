9792735dc5ad288576f467658e3f132bb4aa9fa9
안성재 <sammy0329@naver.com>
2025-01-27 15:06:19 +0900

    feat: 특정 멤버가 만든 contest 리스트 불러오는 API 구현

--- dolharubang/src/main/java/com/dolharubang/repository/ContestRepository.java
+++ dolharubang/src/main/java/com/dolharubang/repository/ContestRepository.java
@@ -1,8 +1,12 @@
 package com.dolharubang.repository;
 
 import com.dolharubang.domain.entity.Contest;
+import com.dolharubang.domain.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContestRepository extends JpaRepository<Contest, Long> {
 
+    List<Contest> findAllByMember(Member member);
+    
 }
