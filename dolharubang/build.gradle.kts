330fb9c8cc7c4e334d8cb733d88d1aa8bc1c62fb
woojin cheon <qpqpqp0102@naver.com>
2025-03-18 22:41:37 +0900

    feat: CustomRequestEntityConvertor를 위한 의존성 주입

--- dolharubang/build.gradle.kts
+++ dolharubang/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
     implementation("com.nimbusds:nimbus-jose-jwt:9.23")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation ("org.bouncycastle:bcpkix-jdk18on:1.72")
 
     configurations.all {
         exclude(group = "commons-logging")
