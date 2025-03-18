62596db047a4c803a6625267bc664fa853ef04c9
woojin cheon <qpqpqp0102@naver.com>
2025-03-18 21:28:36 +0900

    fix: 미사용 필드 삭제, 소셜로그인 위한 필드 추가

--- dolharubang/src/main/java/com/dolharubang/domain/dto/response/member/MemberResDto.java
+++ dolharubang/src/main/java/com/dolharubang/domain/dto/response/member/MemberResDto.java
@@ -1,7 +1,8 @@
 package com.dolharubang.domain.dto.response.member;
 
 import com.dolharubang.domain.entity.Member;
-import java.time.LocalDateTime;
+import com.dolharubang.type.Authority;
+import com.dolharubang.type.Provider;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -12,24 +13,26 @@ import lombok.ToString;
 public class MemberResDto {
 
     private Long memberId;
+    private Authority authority;
+    private Provider provider;
+    private String providerId;
     private String memberEmail;
     private String nickname;
     private String birthday;
     private int sands;
-    private LocalDateTime lastLoginAt;
-    private int totalLoginDays;
     private String profilePicture;
     private String spaceName;
 
     public static MemberResDto fromEntity(Member member) {
         return MemberResDto.builder()
             .memberId(member.getMemberId())
+            .authority(member.getAuthority())
+            .provider(member.getProvider())
+            .providerId(member.getProviderId())
             .memberEmail(member.getMemberEmail())
             .nickname(member.getNickname())
             .birthday(member.getBirthday())
             .sands(member.getSands())
-            .lastLoginAt(member.getLastLoginAt())
-            .totalLoginDays(member.getTotalLoginDays())
             .profilePicture(member.getProfilePicture())
             .spaceName(member.getSpaceName())
             .build();
