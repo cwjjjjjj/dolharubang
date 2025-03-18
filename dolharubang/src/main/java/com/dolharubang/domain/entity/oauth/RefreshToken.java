4b1f5f02a41d60cd75d79994e1d69da7ff2ced48
woojin cheon <qpqpqp0102@naver.com>
2025-03-18 23:17:42 +0900

    fix: 어노테이션 수정

--- dolharubang/src/main/java/com/dolharubang/domain/entity/oauth/RefreshToken.java
+++ dolharubang/src/main/java/com/dolharubang/domain/entity/oauth/RefreshToken.java
@@ -4,8 +4,6 @@ import com.dolharubang.domain.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -19,13 +17,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Table(name = "refresh_token")
 @Entity
 public class RefreshToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
