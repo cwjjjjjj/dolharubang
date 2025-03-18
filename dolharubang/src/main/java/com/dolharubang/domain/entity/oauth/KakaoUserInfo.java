9034de80adba7d1a1b8a9e00c76ac98f37779878
woojin cheon <qpqpqp0102@naver.com>
2025-03-18 22:51:07 +0900

    feat: kakao에서 유저 정보 가지고 오기 위한 클래스

---
+++ dolharubang/src/main/java/com/dolharubang/domain/entity/oauth/KakaoUserInfo.java
@@ -0,0 +1,40 @@
+package com.dolharubang.domain.entity.oauth;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class KakaoUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+
+    public KakaoUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public Optional<String> getEmail() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        return Optional.ofNullable(kakaoAccount)
+            .map(account -> (String) account.get("email"));
+    }
+
+    public Optional<String> getProfilePicture() {
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        return Optional.ofNullable(properties)
+            .map(props -> (String) props.get("profile_image"));
+    }
+}
