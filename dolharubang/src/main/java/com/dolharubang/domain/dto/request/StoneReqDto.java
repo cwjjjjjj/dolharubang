5a0645204e1e59dcbbbe21f0841536d52281caba
woojin cheon <qpqpqp0102@naver.com>
2025-04-29 04:47:24 +0900

    fix: 돌 생성 시 memberId 불필요하도록 수정

--- dolharubang/src/main/java/com/dolharubang/domain/dto/request/StoneReqDto.java
+++ dolharubang/src/main/java/com/dolharubang/domain/dto/request/StoneReqDto.java
@@ -1,21 +1,18 @@
 package com.dolharubang.domain.dto.request;
 
-import com.dolharubang.domain.entity.Member;
 import com.dolharubang.domain.entity.Stone;
 import lombok.Getter;
 
 @Getter
 public class StoneReqDto {
 
-    private Long memberId;
     private Long speciesId;
     private String stoneName;
     private Long closeness;
     private String signText;
 
-    public static Stone toEntity(Member member, StoneReqDto dto) {
+    public static Stone toEntity(StoneReqDto dto) {
         return Stone.builder()
-            .member(member)
             .speciesId(dto.getSpeciesId())
             .stoneName(dto.getStoneName())
             .closeness(dto.getCloseness())
