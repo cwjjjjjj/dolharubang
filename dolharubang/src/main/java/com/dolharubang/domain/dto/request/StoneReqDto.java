a153359f4027bf7b0981dd53a74055ed6fe34c99
woojin cheon <qpqpqp0102@naver.com>
2025-04-30 13:01:24 +0900

    feat: 돌 입양 dto와 관련 메서드 수정

--- dolharubang/src/main/java/com/dolharubang/domain/dto/request/StoneReqDto.java
+++ dolharubang/src/main/java/com/dolharubang/domain/dto/request/StoneReqDto.java
@@ -1,22 +1,11 @@
 package com.dolharubang.domain.dto.request;
 
-import com.dolharubang.domain.entity.Stone;
 import lombok.Getter;
 
 @Getter
 public class StoneReqDto {
 
-    private Long speciesId;
+    private String speciesName;
     private String stoneName;
-    private Long closeness;
-    private String signText;
 
-    public static Stone toEntity(StoneReqDto dto) {
-        return Stone.builder()
-            .speciesId(dto.getSpeciesId())
-            .stoneName(dto.getStoneName())
-            .closeness(dto.getCloseness())
-            .signText(dto.getSignText())
-            .build();
-    }
 }
