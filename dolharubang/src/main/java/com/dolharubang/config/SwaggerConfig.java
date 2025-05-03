52ddfb5f187187c08abb90bf286e5adbbc235263
안성재 <sammy0329@naver.com>
2025-05-03 15:08:46 +0900

    feat: 스웨거에서 전역으로 어세스토큰 받을 수 있도록 수정

--- dolharubang/src/main/java/com/dolharubang/config/SwaggerConfig.java
+++ dolharubang/src/main/java/com/dolharubang/config/SwaggerConfig.java
@@ -1,13 +1,18 @@
 package com.dolharubang.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -18,6 +23,14 @@ public class SwaggerConfig {
                 .description("API documentation for the Schedule Management System")
                 .contact(new Contact()
                     .name("돌하루방")
-                    .url("https://www.dolharubang.store")));
+                    .url("https://www.dolharubang.store")))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                    new SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
     }
 }
