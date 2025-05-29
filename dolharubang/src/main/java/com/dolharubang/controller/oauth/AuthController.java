e0fb021caaffd5759cf6e7da8b2581c9564b3e06
안성재 <sammy0329@naver.com>
2025-05-29 16:37:52 +0900

    fix: 첫 가입시 100개의 모래알 받도록 수정

--- dolharubang/src/main/java/com/dolharubang/controller/oauth/AuthController.java
+++ dolharubang/src/main/java/com/dolharubang/controller/oauth/AuthController.java
@@ -77,7 +77,7 @@ public class AuthController {
                 .memberEmail(kakaoInfo.getEmail())
                 .nickname(kakaoInfo.getNickname())
                 .profilePicture(null)
-                .sands(0)
+                .sands(100)
                 .closeness(0)
                 .build();
 
@@ -162,7 +162,7 @@ public class AuthController {
                 .providerId(providerId)
                 .memberEmail(email)
                 .nickname(nickname)
-                .sands(0)
+                .sands(100)
                 .closeness(0)
                 .build();
 
