b1977237b75c23688017dca744e7f66339d24422
안성재 <sammy0329@naver.com>
2025-06-09 01:01:50 +0900

    feat: 카테고리에 따라 미션 검색하는 쿼리 메서드 추가

--- dolharubang/src/main/java/com/dolharubang/repository/MemberMissionRepository.java
+++ dolharubang/src/main/java/com/dolharubang/repository/MemberMissionRepository.java
@@ -3,6 +3,7 @@ package com.dolharubang.repository;
 import com.dolharubang.domain.entity.Member;
 import com.dolharubang.domain.entity.MemberMission;
 import com.dolharubang.domain.entity.Mission;
+import com.dolharubang.type.MissionCategory;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +20,7 @@ public interface MemberMissionRepository extends JpaRepository<MemberMission, Lo
     void deleteByMemberAndMissionNotIn(Member member, List<Mission> missions);
 
     void deleteAllByMember(Member member);
+
+    List<MemberMission> findByMemberAndMission_Condition_Category(Member member,
+        MissionCategory category);
 }
