0fef68d2d943ce6ab4705d567b30fadfbf6f30d6
안성재 <sammy0329@naver.com>
2025-06-11 14:32:20 +0900

    fix: 친구 관련 이벤트 타입 추가 (친구 요청 보내기, 친구 요청 받기)

--- dolharubang/src/main/java/com/dolharubang/type/FriendActionType.java
+++ dolharubang/src/main/java/com/dolharubang/type/FriendActionType.java
@@ -3,5 +3,7 @@ package com.dolharubang.type;
 public enum FriendActionType {
     ADD,      // 친구 추가
     REJECT,   // 친구 요청 거절
-    REMOVE    // 친구 삭제
+    REMOVE,    // 친구 삭제
+    SEND_REQUEST,   // 친구 요청 보내기
+    RECEIVE_REQUEST // 친구 요청 받기
 }
