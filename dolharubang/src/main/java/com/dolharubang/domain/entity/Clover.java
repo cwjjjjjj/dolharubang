010415a2fc3ec3fa7233d5e7201617277027811a
woojincheon <qpqpqp0102@naver.com>
2025-06-20 17:39:13 +0900

    feat: 어노테이션, 메서드 추가

--- dolharubang/src/main/java/com/dolharubang/domain/entity/Clover.java
+++ dolharubang/src/main/java/com/dolharubang/domain/entity/Clover.java
@@ -1,18 +1,15 @@
 package com.dolharubang.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-@NoArgsConstructor
+import java.time.LocalDateTime;
+
 @Entity
+@Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Clover extends BaseEntity {
 
     @Id
@@ -20,13 +17,26 @@ public class Clover extends BaseEntity {
     private Long cloverId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
+    @JoinColumn(name = "sending_member_id", nullable = false)
     private Member sendingMemberId;
 
-    private Long receivingMemberId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiving_member_id", nullable = false)
+    private Member receivingMemberId;
 
-    private boolean isReplied;
-
+    //TODO 답장 여부 추후 구현
+//    private boolean isReplied;
     //createdAt이 보낸 시간
     //modifiedAt이 답장 시간
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.modifiedAt = LocalDateTime.now();
+    }
 }
