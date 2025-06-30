14ec97a87586c3227bf31f4d011a8e0fd98945aa
woojin cheon <qpqpqp0102@naver.com>
2025-06-30 18:01:01 +0900

    fix: 매핑 주소 수정

--- dolharubang/src/main/java/com/dolharubang/controller/CloverController.java
+++ dolharubang/src/main/java/com/dolharubang/controller/CloverController.java
@@ -7,15 +7,19 @@ import com.dolharubang.domain.entity.oauth.PrincipalDetails;
 import com.dolharubang.service.CloverService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Tag(name = "Clovers", description = "APIs for managing clovers")
@@ -77,7 +81,7 @@ public class CloverController {
     }
 
     @Operation(summary = "내가 받은 클로버 리스트 조회", description = "받은 클로버 전체 조회, API 테스트용")
-    @GetMapping("/sent")
+    @GetMapping("/received")
     public ResponseEntity<?> getReceivedCloverList(@AuthenticationPrincipal PrincipalDetails principal) {
         if (principal == null) {
             return ResponseEntity
