8b3e18b4b3b755a33335f695fd2b6c2a6b17fafd
woojincheon <qpqpqp0102@naver.com>
2025-07-05 01:01:53 +0900

    feat: 메서드명 수정

--- dolharubang/src/main/java/com/dolharubang/repository/CloverRepository.java
+++ dolharubang/src/main/java/com/dolharubang/repository/CloverRepository.java
@@ -2,9 +2,10 @@ package com.dolharubang.repository;
 
 import com.dolharubang.domain.entity.Clover;
 import com.dolharubang.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CloverRepository extends JpaRepository<Clover, Long> {
 
@@ -17,5 +18,5 @@ public interface CloverRepository extends JpaRepository<Clover, Long> {
     //같은 회원에게 하루 하나 전송 + 하루에 총 7개 전송 제한을 위한 리스트
     List<Clover> findBySendingMemberAndCreatedAtBetween(Member sendingMember, LocalDateTime start, LocalDateTime end);
 
-    boolean existsBySendingMemberAndReceivingMemberAndCreatedAtBetween(Member sendingMember, Member receivingMember, LocalDateTime start, LocalDateTime end);
+    boolean existsBySendingMemberAndReceivingMemberAndCreatedAtAfter(Member sendingMember, Member receivingMember, LocalDateTime start);
 }
